7f34e823b322f99b4179cc67f19032037a9827c1
Ossama Essmiri <oessmiri@c3r8p5.1337.ma>
2025-02-19 14:22:21 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oessmiri <oessmiri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 08:27:00 by oessmiri          #+#    #+#             */
+/*   Updated: 2024/12/01 15:14:22 by oessmiri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# if BUFFER_SIZE < 0
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_strdup(const char *s);
+int		ft_strlen(char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+char	*read_file(int fd, char *storage);
+char	*extract_line(char *storage);
+char	*update_storage(char *storage);
+
+#endif
